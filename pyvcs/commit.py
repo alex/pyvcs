58bad1e1baf4d1d3c88f6a69c928d08bc1cf2cb0
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-01 22:30:39 -0500

    get the git backend working

--- pyvcs/commit.py
+++ pyvcs/commit.py
@@ -1,5 +1,9 @@
 class Commit(object):
     def __init__(self, author, time, message, diff):
+        """
+        author should be a string, time a datetime object, message a string,
+        and diff a string
+        """
         self.author = author
         self.time = time
         self.message = message
