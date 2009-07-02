fbed4e427195f95cd54db38cac16014321ea0000
Justin Lilly <justinlilly@gmail.com>
2009-07-02 01:08:38 -0400

    Adding some basic working tests to the hg bits.

---
+++ tests/justin_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from datetime import datetime
+import unittest
+
+from pyvcs.backends import get_backend
+from pyvcs.exceptions import FileDoesNotExist, FolderDoesNotExist
+
+
+class HGTest(unittest.TestCase):
+    def setUp(self):
+        hg = get_backend('hg')
+        self.repo = hg.Repository('/home/jlilly/Code/python/pyvcs/src/mercurial')
+        
+    def test_commits(self):
+        commit = self.repo.get_commit_by_id(45)
+        self.assert_(commit.author.startswith('mpm'))
+        self.assertEqual(commit.time, datetime(2005, 5, 10, 4, 34, 57))
+        self.assert_(commit.message.startswith('Fix recursion depth'))
+
+if __name__ == '__main__':
+    unittest.main()
