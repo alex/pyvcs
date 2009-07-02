7243c6efc1141d37cd66b7273f0055533c177323
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-01 22:44:06 -0500

    a commit has an associate list of files as well

--- pyvcs/commit.py
+++ pyvcs/commit.py
@@ -1,8 +1,8 @@
 class Commit(object):
-    def __init__(self, author, time, message, diff):
+    def __init__(self, author, time, message, files, diff):
         """
         author should be a string, time a datetime object, message a string,
-        and diff a string
+        files a list of filenames (strings), and diff a string
         """
         self.author = author
         self.time = time
