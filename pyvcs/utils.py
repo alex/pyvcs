7f0853522424ceefc44d57f7c597dee2df6f2fe7
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-03 16:11:05 -0500

    include the revisions in the generated diffs

--- pyvcs/utils.py
+++ pyvcs/utils.py
@@ -15,6 +15,6 @@ def generate_unified_diff(repository, changed_files, commit1, commit2):
             file2 = ''
         diffs.append(unified_diff(
             file1.splitlines(), file2.splitlines(), fromfile=file_name,
-            tofile=file_name
+            tofile=file_name, fromfiledate=commit1, tofiledate=commit2
         ))
     return '\n'.join('\n'.join(map(lambda s: s.rstrip('\n'), diff)) for diff in diffs)
