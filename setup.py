3b7ea535700f30d1c7a7567ce35e1e1dc920fd37
Justin Lilly <justinlilly@gmail.com>
2009-07-04 01:49:03 -0400

    Adding a basic setup.py for submission to pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyvcs',
+    version = '0.1',
+    packages = find_packages('pyvcs'),
+    package_dir = {'': 'pyvcs'},
+    install_requires = ['setuptools'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Version Control'
+        ],
+)
