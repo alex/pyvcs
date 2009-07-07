2a2e11f6be46bd10c787738f195fef869149fc4e
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@blackchairsoft.com>
2009-07-06 18:17:53 -0700

    Added list_directory to subversion backend
    Along with comments and aclarations of the implementation
    and its current limitations.

--- pyvcs/backends/subversion.py
+++ pyvcs/backends/subversion.py
@@ -9,6 +9,14 @@ from pyvcs.exceptions import CommitDoesNotExist, FileDoesNotExist, FolderDoesNot
 from pyvcs.repository import BaseRepository
 from pyvcs.utils import generate_unified_diff
 
+#TODO: Fix path handling, with svn you have 2 paths, the local
+# one (working local copy) and the remote (svn server).
+# Local: /home/user/myrepo
+# Remote: svn.myserver.com/svn/myrepo/trunk
+# The problem is that remote path will be /myrepo/trunk,
+# this brings problems when quering svn for file information,
+# lists and logs. Assuming you are using a local working copy
+# everything should more or less work fine.
 class Repository(BaseRepository):
     def __init__(self, path, **kwargs):
         """
@@ -71,7 +79,31 @@ class Repository(BaseRepository):
         Returns a tuple of lists of files and folders in a given directory at a
         given revision, or HEAD if revision is None.
         """
-        raise NotImplementedError
+        if revision:
+            rev = pysvn.Revision(pysvn.opt_revision_kind.number, revision)
+        else:
+            rev = pysvn.Revision(pysvn.opt_revision_kind.head)
+        
+        dir_path = os.path.join(self.path, path)
+        
+        try:
+            entries = self.client.list(dir_path, revision=rev, recurse=False)
+        except pysvn.ClientError: 
+            raise FolderDoesNotExist
+        
+        files, folders = [], []
+        for file_info, file_pops in entries:
+            if file_info['kind'] == pysvn.node_kind.dir:
+                #TODO: Path is not always present, only repos_path
+                # is guaranteed, in case of looking at a remote
+                # repository (with no local working copy) we should
+                # check against repos_path.
+                if not dir_path.startswith(file_info['path']):
+                    folders.append(os.path.basename(file_info['repos_path']))
+            else:
+                files.append(os.path.basename(file_info['repos_path']))
+            
+        return files, folders
 
     def file_contents(self, path, revision=None):
         """
